1112333c66878f1e3964bd1aa21cffa27ce9d455
rafaela <rafaellapereyra@gmail.com>
2018-09-03 01:14:08 -0300

    Atualizando scripts do banco

--- DataBase/scripts.sql
+++ DataBase/scripts.sql
@@ -1,72 +1,85 @@
-CREATE TABLE usuario(
-    idUsuario int,
-    tipoUsuario varchar(20) NOT NULL,
-    nome varchar(200) NOT NULL,
-    fotoPerfil varchar(200) NOT NULL,
-    telefone varchar(15),
-    sexo varchar(15),
-    email varchar(200) NOT NULL,
-    profissao varchar(100),
-    dataNascimento DATE,
-    categoriaEstabelecimento varchar(100),
-    nota numeric(1,1),
-    descricao varchar(300),
-    rua varchar(200) NOT NULL,
-    estado varchar(200) NOT NULL,
-    cidade varchar(200) NOT NULL,
-    cep varchar(15) NOT NULL,
-    username varchar(200) NOT NULL UNIQUE,
-    senha varchar(200) NOT NULL,
-    PRIMARY KEY(idUsuario)
+
+CREATE TABLE Usuario (
+idUsuario serial,
+nome VARCHAR (200) NOT NULL,
+email VARCHAR(200) NOT NULL UNIQUE,
+senha VARCHAR(15) NOT NULL,
+sexo VARCHAR (15),
+dataNascimento DATE NOT NULL,
+rua varchar(200),
+cidade varchar(50),
+cep varchar(20),
+numeroCasa varchar(5),
+estado varchar (3),
+telefone varchar(15) ,
+profissao varchar(100),
+fotoPerfil bytea NOT NULL,
+PRIMARY KEY(idUsuario)  );
+
+
+CREATE TABLE Estabelecimento(
+idEstabelecimento serial,
+idUsuario int ,
+nome varchar(200) NOT NULL,
+telefone varchar(20),
+fotoEstabelecimento bytea NOT NULL,
+cartegoria varchar (100) NOT NULL,
+nota numeric(1,1),
+descricao varchar(200),
+rua varchar(200) NOT NULL,
+estado varchar(10) NOT NULL,
+cidade varchar(200) NOT NULL,
+cep varchar(10) ,
+dia varchar(30) NOT NULL,
+horaAbre TIME NOT NULL,
+horaFecha TIME NOT NULL,
+PRIMARY KEY(idEstabelecimento),
+FOREIGN KEY(idUsuario) REFERENCES Usuario(idUsuario)
+
+
+
 );
 
-CREATE TABLE horaFuncionamento(
-    idFuncionamento int,
-    idUsuario int NOT NULL,
-    dia varchar(30) NOT NULL,
-    horaAbre DATE NOT NULL,
-    horaFecha DATE NOT NULL,
-    PRIMARY KEY(idFuncionamento),
-    FOREIGN KEY(idUsuario) REFERENCES Usuario(idUsuario)
-);
+
+
 
 CREATE TABLE Comida(
-    idComida int,
+    idComida serial,
     descricao varchar(300) NOT NULL,
     nota numeric(1,1),
     preco float NOT NULL,
     nome varchar(200) NOT NULL,
     idUsuario int,
-    horaFuncionamento int,
+    idEstabelecimento int,
     PRIMARY KEY(idComida),
-    FOREIGN KEY(horaFuncionamento) REFERENCES horaFuncionamento(idFuncionamento),
+    FOREIGN KEY(idEstabelecimento) REFERENCES Estabelecimento(idEstabelecimento),
     FOREIGN KEY(idUsuario) REFERENCES Usuario(idUsuario)
 );
 
 
 CREATE TABLE Chekin(
-    idChekin int,
+    idChekin serial,
     horario TIMESTAMP,
     consumidor int NOT NULL,
     estabelecimento int NOT NULL,
     PRIMARY KEY(idChekin),
     FOREIGN KEY(consumidor) REFERENCES Usuario(idUsuario),
-    FOREIGN KEY(estabelecimento) REFERENCES Usuario(idUsuario)
+    FOREIGN KEY(estabelecimento) REFERENCES Estabelecimento(idEstabelecimento)
 );
 
 CREATE TABLE AvaliarEstabelecimento(
-    idAvaliacao int,
+    idAvaliacao serial,
     consumidor int NOT NULL,
     estabelecimento int NOT NULL,
     nota numeric(1,1) NOT NULL,
     comentario varchar(500),
     PRIMARY KEY(idAvaliacao),
     FOREIGN KEY(consumidor) REFERENCES Usuario(idUsuario),
-    FOREIGN KEY(estabelecimento) REFERENCES Usuario(idUsuario)
-);
+    FOREIGN KEY(estabelecimento) REFERENCES Estabelecimento(idEstabelecimento)
+    );
 
 CREATE TABLE AvaliarComida(
-    idAvaliacao int,
+    idAvaliacao serial,
     consumidor int NOT NULL,
     estabelecimento int NOT NULL,
     nota numeric(1,1) NOT NULL,
@@ -74,12 +87,12 @@ CREATE TABLE AvaliarComida(
     comida int,
     PRIMARY KEY(idAvaliacao),
     FOREIGN KEY(consumidor) REFERENCES Usuario(idUsuario),
-    FOREIGN KEY(estabelecimento) REFERENCES Usuario(idUsuario),
+    FOREIGN KEY(estabelecimento) REFERENCES Estabelecimento(idEstabelecimento),
     FOREIGN KEY(comida) REFERENCES Comida(idComida)
 );
 
 CREATE TABLE RecomendarComida(
-    idRecomendacao int,
+    idRecomendacao serial,
     comida int NOT NULL,
     remetente int NOT NULL,
     destinatario int NOT NULL,
@@ -91,7 +104,7 @@ CREATE TABLE RecomendarComida(
 );
 
 CREATE TABLE Mensagem(
-    idMensagem int,
+    idMensagem serial,
     remetente int NOT NULL,
     destinatario int NOT NULL,
     mensagem varchar(500) NOT NULL,
@@ -102,7 +115,7 @@ CREATE TABLE Mensagem(
 );
 
 CREATE TABLE SolicitaAmizade(
-    idSolicitacao int,
+    idSolicitacao serial,
     remetente int NOT NULL,
     destinatario int NOT NULL,
     horario TIMESTAMP,
@@ -110,3 +123,4 @@ CREATE TABLE SolicitaAmizade(
     FOREIGN KEY(destinatario) REFERENCES Usuario(idUsuario),
     FOREIGN KEY(remetente) REFERENCES Usuario(idUsuario)
 );
+
